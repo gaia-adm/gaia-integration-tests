7fb92ffa1f342c977ff8726e5f7b622714505980
kornfeld <lital.kornfeld@hpe.com>
2016-04-03 09:20:50 +0300

    delete index by its name (not pattern); stop running test in circleci for now

--- src/test/java/com/adm/gaia/webhook/TestWebhookGenerator.java
+++ src/test/java/com/adm/gaia/webhook/TestWebhookGenerator.java
@@ -4,6 +4,7 @@ import com.adm.gaia.elasticsearch.ElasticSearchManagement;
 import com.adm.gaia.elasticsearch.SearchQuery;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.search.SearchHit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,7 @@ public class TestWebhookGenerator extends GaiaTestCase {
     @Autowired
     private ElasticSearchManagement _elasticSearchManagement;
     private String _indexPattern = "gaia*github.push";
+    private String _index;
     
     @Test
     public void testGenerate() throws Exception {
@@ -39,15 +41,17 @@ public class TestWebhookGenerator extends GaiaTestCase {
         SearchResponse response = new SearchQuery(_elasticSearchManagement, _indexPattern).
                 search("head_commit.message:Update README.md");
         Assert.assertTrue(response.getHits().totalHits() > 0);
-        String head_commit = response.getHits().getAt(0).getSource().get("head_commit").toString();
+        SearchHit hit = response.getHits().getAt(0);
+        String head_commit = hit.getSource().get("head_commit").toString();
         Assert.assertTrue(head_commit.contains("0d1a26e67d8f5eaf1f6ba5c57fc3c7d91ac0fd1c"));
+        _index = hit.index();
     }
 
     @AfterMethod
     private void deleteIndices() {
 
-        _logger.debug("deleting indices of pattern: " + _indexPattern);
-        DeleteIndexRequest request = new DeleteIndexRequest(_indexPattern);
+        _logger.debug("deleting index: " + _index);
+        DeleteIndexRequest request = new DeleteIndexRequest(_index);
         boolean acknowledged = _elasticSearchManagement.getClient().admin().indices().delete(request).actionGet().isAcknowledged();
         _logger.debug("acknowledged: " + acknowledged);
     }

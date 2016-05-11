f9f0a123ccaeedffff1d52cc83b986cee759c899
Effi <effoeffi@gmail.com>
2016-05-11 17:21:19 +0300

    RetriableOperationExecutor created for searching on created index

--- src/main/java/com/adm/gaia/elasticsearch/ElasticSearchHttpClient.java
+++ src/main/java/com/adm/gaia/elasticsearch/ElasticSearchHttpClient.java
@@ -3,6 +3,7 @@ package com.adm.gaia.elasticsearch;
 import com.adm.gaia.rest.RestClient;
 import com.adm.gaia.rest.RestRequest;
 import com.adm.gaia.rest.RestResponse;
+import com.adm.gaia.util.RetriableOperationExecutor;
 import com.adm.gaia.webhook.GaiaUrlContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,14 +15,14 @@ import java.net.URLEncoder;
 @Component
 public class ElasticSearchHttpClient {
 
-    private static final Logger _logger = LoggerFactory.getLogger(ElasticSearchManagement.class);
+    private static final Logger _logger = LoggerFactory.getLogger(ElasticSearchHttpClient.class);
 
     @Autowired
     private GaiaUrlContainer _urlContainer;
 
     public String search(String index, String query) {
 
-        String ret = "";
+        String ret;
         try {
             query = URLEncoder.encode(query, "UTF-8");
         } catch (Exception e) {
@@ -29,7 +30,12 @@ public class ElasticSearchHttpClient {
         }
         String url = String.format("%s%s/_search?q=%s", _urlContainer.getGaiaESUrl(), index, query);
         try {
-            ret = RestClient.get(new RestRequest(url)).getResponseBody();
+            RestResponse response = RetriableOperationExecutor.execute(
+                    Void -> RestClient.get(new RestRequest(url)),
+                    exception -> exception.getMessage().contains("index_not_found_exception"),
+                    5000,
+                    3);
+            ret = response.getResponseBody();
             _logger.debug(String.format("Search URL: %s, Results: %s", url, ret));
         } catch (Exception e) {
             throw new RuntimeException("Failed to search for created webhook index in ES, url: "
---
+++ src/main/java/com/adm/gaia/util/RetriableOperationExecutor.java
@@ -0,0 +1,32 @@
+package com.adm.gaia.util;
+
+
+import java.util.function.Function;
+
+public class RetriableOperationExecutor {
+
+    public static <TResult> TResult execute(
+            Function<Void, TResult> delegate,
+            Function<Exception, Boolean> isValid,
+            int sleepTime, int retries) {
+
+        int count = 0;
+        while (true) {
+            try {
+                return delegate.apply(null);
+            } catch (Exception e) {
+                if(!isValid.apply(e)) {
+                    throw e;
+                }
+                if (count++ > retries) {
+                    throw e;
+                }
+                try {
+                    Thread.sleep(sleepTime);
+                } catch (InterruptedException ie) {
+                    throw new RuntimeException(ie);
+                }
+            }
+        }
+    }
+}

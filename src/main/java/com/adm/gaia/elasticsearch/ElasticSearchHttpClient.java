3f944ae2c0cd23c5986d2733b009678b928e396a
Effi <effoeffi@gmail.com>
2016-05-11 15:35:53 +0300

    elastic search http client - search added

---
+++ src/main/java/com/adm/gaia/elasticsearch/ElasticSearchHttpClient.java
@@ -0,0 +1,41 @@
+package com.adm.gaia.elasticsearch;
+
+import com.adm.gaia.rest.RestClient;
+import com.adm.gaia.rest.RestRequest;
+import com.adm.gaia.rest.RestResponse;
+import com.adm.gaia.webhook.GaiaUrlContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.net.URLEncoder;
+
+@Component
+public class ElasticSearchHttpClient {
+
+    private static final Logger _logger = LoggerFactory.getLogger(ElasticSearchManagement.class);
+
+    @Autowired
+    private GaiaUrlContainer _urlContainer;
+
+    public String search(String index, String query) {
+
+        String ret = "";
+        try {
+            query = URLEncoder.encode(query, "UTF-8");
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to encode ES search query: " + query, e);
+        }
+        String url = String.format("%s%s/_search?q=%s", _urlContainer.getGaiaESUrl(), index, query);
+        try {
+            ret = RestClient.get(new RestRequest(url)).getResponseBody();
+            _logger.debug(String.format("Search URL: %s, Results: %s", url, ret));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to search for created webhook index in ES, url: "
+                                       + url, e);
+        }
+
+        return ret;
+    }
+}
